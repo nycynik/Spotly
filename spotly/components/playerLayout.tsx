9ea93707227eb3f3a3a3301cc3c0ad55ee564a49
Mike Lynch <nycynik@gmail.com>
2022-08-16 15:25:13 -0700

    added basic component for fixed layout

---
+++ spotly/components/playerLayout.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/layout';
+
+const PlayerLayout = ({ children }) => {
+    return <Box>
+        Lyaout here.
+        {children}
+  </Box>;
+};
+
+export default PlayerLayout;
--- spotly/pages/_app.tsx
+++ spotly/pages/_app.tsx
@@ -1,19 +1,23 @@
-import { ChakraProvider, extendTheme } from '../node_modules/@chakra-ui/react/dist/chakra-ui-react.cjs';
-import 'reset-css';
+import {
+  ChakraProvider,
+  extendTheme,
+} from "@chakra-ui/react/dist/chakra-ui-react.cjs";
+import "reset-css";
+import PlayerLayout from "../components/playerLayout";
 
 const spotTheme = extendTheme({
   colors: {
     gray: {
-      100: '#f5f5f5',
-      200: '#eeeeee',
-      300: '#e0e0e0',
-      400: '#bdbdbd',
-      500: '#9e9e9e',
-      600: '#757575',
-      700: '#616161',
-      800: '#424242',
-      900: '#212121'
-    }
+      100: "#f5f5f5",
+      200: "#eeeeee",
+      300: "#e0e0e0",
+      400: "#bdbdbd",
+      500: "#9e9e9e",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
   },
   components: {
     // Button: {
@@ -26,13 +30,17 @@ const spotTheme = extendTheme({
     //     }
     //   }
     // }
-  }
+  },
 });
 
-function MyApp({ Component, pageProps }) {
-  return <ChakraProvider theme={spotTheme}>
-    <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={spotTheme}>
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
     </ChakraProvider>
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
